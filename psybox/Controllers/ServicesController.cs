5f014503cce4f5ceddbc41b47f6f1c1a79f5401f
Madushanka Fernando <kavindumadushanka90@gmail.com>
2021-11-13 05:47:25 +0530

    commit

---
+++ psybox/Controllers/ServicesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace psybox.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult ESDevelopment()
+        {
+            return View();
+        }
+
+        public IActionResult ITConsulting()
+        {
+            return View();
+        }
+
+        public IActionResult DataExtraction()
+        {
+            return View();
+        }
+
+        public IActionResult AInML()
+        {
+            return View();
+        }
+
+        public IActionResult MADevelopment()
+        {
+            return View();
+        }
+
+        public IActionResult WDevelopment()
+        {
+            return View();
+        }
+    }
+}
---
+++ psybox/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace psybox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
